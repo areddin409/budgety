5d49a3326471def4d2ba99a1e0951ef2fdde8a23
Andrew <areddin409@gmail.com>
2020-02-24 23:55:53 -0600

    Added functionality to delete items from budget list and UI is updated

--- app.js
+++ app.js
@@ -58,6 +58,22 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+      //returns a new array
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      //finds the index for the id that needs to be deleted
+      index = ids.indexOf(id);
+
+      //removes the id
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
       //calc total income and  expense
       calcuateTotal('exp');
@@ -100,7 +116,8 @@ var UIController = (function() {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expenseLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    containter: '.container'
   };
 
   return {
@@ -118,11 +135,11 @@ var UIController = (function() {
       if (type === 'inc') {
         element = DOMstrings.incomeContainer;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === 'exp') {
         element = DOMstrings.expenseContainer;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
       //replace placeholder text with some actual data
@@ -132,6 +149,12 @@ var UIController = (function() {
 
       //insert html into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    //Removes item from either Income or Expenses lists
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -184,6 +207,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.containter)
+      .addEventListener('click', ctrlDeleteItem);
   }
 
   var updateBudget = function() {
@@ -214,6 +241,29 @@ var controller = (function(budgetCtrl, UICtrl) {
       UICtrl.clearFields();
 
       //5. Calc and update budget
+      updateBudget();
+    }
+  };
+
+  var ctrlDeleteItem = function(event) {
+    var itemID, splitID, type, id;
+
+    //DOM Tranversing
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      //inc-1
+      splitID = itemID.split('-');
+      type = splitID[0];
+      id = parseInt(splitID[1]);
+
+      //1. delete item from the data structure
+      budgetCtrl.deleteItem(type, id);
+
+      //2. delete the item from the UI
+      UICtrl.deleteListItem(itemID);
+
+      //3. Update and show the new budget
       updateBudget();
     }
   };
